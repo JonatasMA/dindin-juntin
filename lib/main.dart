448c4818927a3c6b592e1f863224ccded3ce9840
Jonatas Macedo <jonatasmacedo70@hotmail.com>
2022-11-08 00:38:05 -0300

    fix: add ios support

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:dindin_juntin/views/home_page.dart';
 import 'package:dindin_juntin/views/login.dart';
 import 'package:dindin_juntin/views/sign_up.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:dindin_juntin/firebase_options.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp fbApp = await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final FirebaseApp fbApp = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Main());
 }
 
 class Main extends StatelessWidget {
-  
   const Main({super.key});
 
   @override
